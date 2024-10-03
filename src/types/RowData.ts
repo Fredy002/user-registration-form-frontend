c03f3a391433ce28106c8290a13ea7e404801ad3
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-03 18:02:11 -0500

    feat(types): add RowData type definition for user records

---
+++ src/types/RowData.ts
@@ -0,0 +1,10 @@
+export type RowData = {
+    id?: number;
+    nombre: string;
+    apellidos: string;
+    sexo: string;
+    fecha_nacimiento: string;
+    edad: string;
+    estado: string;
+    accion: string;
+};
