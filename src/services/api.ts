14aee7ad09f90946bf9da18b2d34ccab6566be4f
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-03 21:06:38 -0500

    update url-api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://crud-api-laravel-production.up.railway.app/api', // URL del backend
+    //baseURL: 'https://crud-api-laravel-production.up.railway.app/api', // URL del backend
+    baseURL: 'http://localhost:8000/api', // URL del backend
 });
 
 export default api;
